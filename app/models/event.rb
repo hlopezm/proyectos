485e5d47f45dfcad17a93646a817dd78c9e57f5e
Jorge Dias <jorge@mrdias.com>
2014-03-11 17:54:31 +0100

    TDD

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,44 @@
 class Event < ActiveRecord::Base
+#  validates :name, presence: true
+  # validates_presence_of :name
+  # validates_length_of :name, maximum: 60
+
+  validates :name, presence: true, length: { maximum: 60 }
+
+  #validates :description, length: { minimum: 100, if: :description_present? }
+
+  validates_length_of :description, minimum: 100, allow_blank: true
+
+  validate :start_at_is_present
+
+  def self.for_today
+    where(["DATE(start_at) = ?", Date.today])
+#    where(start_at: Date.today, end_at: 1.day.from_now, description: ["hi", "hello"])
+  end
+
+  # scope :already_started, lambda {
+  #   where(["start_at < ?", Time.now])
+  # }
+
+  # scope :name_like, lambda { |name|
+  #   where(["name like ?", "%#{name}%"])
+  # }
+
+  # def self.description_like(description)
+  #   where(["description like ?", "%#{description}%"])
+  # end
+
+  # private
+
+  # def description_present?
+  #   description.present?
+  # end
+
+  private
+
+  def start_at_is_present
+    if start_at.blank?
+      errors.add(:start_at, "debes introducir la fecha de inicio")
+    end
+  end
 end
---
+++ spec/models/event_spec.rb
@@ -0,0 +1,78 @@
+require 'spec_helper'
+
+describe Event do
+
+  # describe 'name' do
+  #   it 'should not be blank' do
+  #     event = Event.new name: nil
+  #     expect(event).to have(1).error_on(:name)
+  #   end
+
+  #   it 'should not have error when present' do
+  #     event = Event.new name: "hi"
+  #     expect(event).not_to have(1).error_on(:name)
+  #   end
+
+  #   it 'should not be longer than 60 characters' do
+  #     event = Event.new name: "a"*61
+  #     expect(event).to have(1).error_on(:name)
+  #   end
+  # end
+
+  describe 'description' do
+
+    it 'is valid when blank' do
+      event = Event.new description: ""
+      expect(event).to have(0).error_on(:description)
+    end
+
+    it 'is not valid when it has less than 100 characters' do
+      event = Event.new description: "a"*99
+      expect(event).to have(1).error_on(:description)
+    end
+
+    it 'is valid when it has 100 characters' do
+      event = Event.new description: "a"*100
+      expect(event).to have(0).error_on(:description)
+    end
+
+  end
+
+  describe 'start_at' do
+
+    it 'is invalid when blank' do
+      event = Event.new start_at: nil
+      expect(event).to have(1).error_on(:start_at)
+    end
+
+  end
+
+  describe 'for_today' do
+
+    it 'returns an event that starts today' do
+      event = FactoryGirl.create(:event, start_at: Time.now)
+      expect(Event.for_today).to include(event)
+    end
+
+    it 'does not return an event that starts tomorrow' do
+      event = FactoryGirl.create(:event, start_at: 1.day.from_now)
+      expect(Event.for_today).to be_empty
+      # expect(Event.for_today).not_to include(event)
+
+      # expect(Event.for_today).to eq([])
+      # expect(Event.for_today).not_to exist
+      # expect(Event.for_today.count).to eq(0)
+      # expect(Event.for_today.count).to be_zero
+    end
+
+    it 'does not return an event that has started' do
+      event = FactoryGirl.create(:event, start_at: 1.day.ago)
+      expect(Event.for_today).to be_empty
+    end
+
+  end
+
+
+
+
+end
