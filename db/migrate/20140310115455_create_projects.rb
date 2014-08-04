2ab1aed13694638b37769a3c3d7ed516828cf1c5
hlopezm <hlopezm23@gmail.com>
2014-08-04 10:03:14 -0500

    Adding fields necesary to Project table

--- db/migrate/20140310115455_create_projects.rb
+++ db/migrate/20140310115455_create_projects.rb
@@ -5,13 +5,24 @@ class CreateProjects < ActiveRecord::Migration
       t.text :description
       t.string :portfolio
       t.string :image_url
-      t.string :folio
+      t.string :folio_number
       t.string :authorization_number
-      t.string :work_number
+      t.string :contract_number
+      t.string :contract_modified
       t.integer :expected_amount, :precision => 8, :scale => 2
       t.integer :authorized_amount, :precision => 8, :scale => 2
+      t.integer :economy, :precision => 8, :scale => 2
       t.string :location
+      t.integer :goal
+      t.string :unit
       t.string :executor
+      t.string :company
+      t.integer :physical_advance, :precision => 8, :scale => 2
+      t.integer :financial_advance, :precision => 8, :scale => 2
+      t.string :status
+      t.string :delay
+      t.string :contact
+      t.string :observations
 
       t.datetime :start_at
       t.datetime :end_at
